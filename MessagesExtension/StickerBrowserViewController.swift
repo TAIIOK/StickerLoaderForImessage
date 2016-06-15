3c301171950b60e23092dd78e1b7e95a7c1b8df7
Roman <bolobolbfmv@gmail.com>
2016-06-15 03:46:32 +0300

    First

---
+++ MessagesExtension/StickerBrowserViewController.swift
@@ -0,0 +1,111 @@
+//
+//  StickerBrowserViewController.swift
+//  StikerLoader
+//
+//  Created by Roman Efimov on 15.06.16.
+//  Copyright © 2016 Roman Efimov. All rights reserved.
+//
+
+import Foundation
+import Messages
+import UIKit
+
+class StickerBrowserViewController: MSStickerBrowserViewController {
+    
+    var stickers = [MSSticker]()
+    
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createSticker(name: "yaoming")
+        let url = URL(string: "https://pp.vk.me/c616730/v616730757/fea/Oq5FXiHPi0Y.jpg")
+        LoadStickerFromInternet(url: url!)
+        
+    
+    }
+    func LoadSticker(url : URL )
+    {
+            do {
+                let data = try Data(contentsOf: url)
+                let bundle = Bundle.main()
+                let paths = FileManager.default().urlsForDirectory(.documentDirectory, inDomains: .userDomainMask)
+                var Url : URL
+                do
+                {
+                    let filePath = try paths[0].appendingPathComponent("filename.jpg")
+                    Url = filePath
+                }
+                catch {
+                    fatalError("\(error)")
+                }
+                
+                do {
+                    try  data.write(to: Url)
+                }
+                catch {
+                    fatalError("\(error)")
+                }
+                
+                
+                let sticker: MSSticker = {
+                    
+                    do {
+                        let description = NSLocalizedString("filename", comment: "")
+                        return try MSSticker(contentsOfFileURL: Url, localizedDescription: description)
+                    }
+                    catch {
+                        fatalError(" \(error)")
+                    }
+                }()
+                
+                self.stickers.append(sticker)
+            
+            }
+            catch {
+                fatalError("\(error)")
+            }
+        
+    
+    }
+    
+    
+
+    
+
+    
+
+    func createSticker(name: String) {
+        let sticker: MSSticker = {
+            let bundle = Bundle.main()
+            guard let placeholderURL = bundle.urlForResource(name, withExtension: "png") else {
+                fatalError("Unable to find placeholder  image")
+            }
+            do {
+                let description = NSLocalizedString(name, comment: "")
+                return try MSSticker(contentsOfFileURL: placeholderURL, localizedDescription: description)
+            }
+            catch {
+                fatalError("\(error)")
+            }
+        }()
+        stickers.append(sticker)
+    }
+    
+    override func numberOfStickers(in stickerBrowserView: MSStickerBrowserView) -> Int {
+        return stickers.count
+    }
+    
+    override func stickerBrowserView(_ stickerBrowserView: MSStickerBrowserView,
+                                     stickerAt index: Int) -> MSSticker {
+        return stickers[index]
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# StickerLoader
+Sticker loader imessage 
+
+Functions :
+
+ - createSticker(name: String)  // load sticker from project
+ - LoadSticker(url : URL ) // load image from internet save it and create sticker
+
+To Do :
+
+- Working with Files
+- File storage
+
